89c4555d7c6f072ac8a25dd246cc60b0f82784df
Noah Chapman <ncthehammer03@gmail.com>
2025-12-12 11:33:40 -0700

    Update create_linklist.m

--- create_linklist.m
+++ create_linklist.m
@@ -3,7 +3,6 @@ function linkList = create_linklist()
     radius_inner = 0.04;           % Radius of hollow cylinder (meters)
     radius_outer = 0.05;
     density = 2700;          % Density of aluminum (kg/m^3)
-    mass = density * link_len;  % Mass for links 1, 2, 3
     % append i to linklist
     for i = 1:4
         a_i = dhTable(i, 'a');
@@ -15,6 +14,8 @@ function linkList = create_linklist()
         else    % Inertia tensor calculations
             if a_i == 0 % Links 1 and 2: hollow cylinder along z
                 link_len = d_i;
+                volume = pi * (radius_outer^2 - radius_inner^2) * link_len;
+                mass = density * volume;
                 Ixx = (1/12) * mass * (3*(radius_outer^2 + radius_inner^2) + link_len^2);
                 Iyy = Ixx;
                 Izz = 0.5 * mass * (radius_outer^2 + radius_inner^2);
@@ -22,6 +23,8 @@ function linkList = create_linklist()
                 cent_of_mass = [0; 0; link_len/2];
             else    % Link 3 along x
                 link_len = a_i;
+                volume = pi * (radius_outer^2 - radius_inner^2) * link_len;
+                mass = density * volume;
                 Ixx = 0.5 * mass * (radius_outer^2 + radius_inner^2);
                 Iyy = (1/12) * mass * (3*(radius_outer^2 + radius_inner^2) + link_len^2);
                 Izz = Iyy;
