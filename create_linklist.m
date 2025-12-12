87726caddd0392329246dd6a739e69acfcb5cd73
Noah Chapman <ncthehammer03@gmail.com>
2025-12-12 11:29:29 -0700

    Fixed inertia calcs to use thick-walled hollow cylinder

--- create_linklist.m
+++ create_linklist.m
@@ -1,26 +1,27 @@
 function linkList = create_linklist()
     linkList = [];
+    radius_inner = 0.04;           % Radius of hollow cylinder (meters)
+    radius_outer = 0.05;
+    density = 2700;          % Density of aluminum (kg/m^3)
+    mass = density * link_len;  % Mass for links 1, 2, 3
     % append i to linklist
     for i = 1:4
         a_i = dhTable(i, 'a');
         d_i = dhTable(i, 'd');
         alpha_i = dhTable(i, 'alpha');
 
-        density = 2700;
-        mass = density * link_len;  % Mass for links 1, 2, 3
-
         if i == 4   % It's a laser...no mass or inertia
             link_i = createLink(a_i, [], alpha_i, 0, 0, 0, 0, 0);
         else    % Inertia tensor calculations
             if a_i == 0 % Links 1 and 2: hollow cylinder along z
-                Ixx = (1/12) * m * (3*radius^2 + link_len^2);
+                Ixx = (1/12) * mass * (3*(radius_outer^2 + radius_inner^2) + link_len^2);
                 Iyy = Ixx;
-                Izz = m * radius^2;
+                Izz = 0.5 * mass * (radius_outer^2 + radius_inner^2);
                 inertia_tensor = diag([Ixx, Iyy, Izz]);
                 cent_of_mass = [0; 0; link_len/2];
             else    % Link 3 along x
-                Ixx = m * radius^2;
-                Iyy = (1/12) * m * (3*radius^2 + link_len^2);
+                Ixx = 0.5 * mass * (radius_outer^2 + radius_inner^2);
+                Iyy = (1/12) * mass * (3*(radius_outer^2 + radius_inner^2) + link_len^2);
                 Izz = Iyy;
                 inertia_tensor = diag([Ixx, Iyy, Izz]);
                 cent_of_mass = [link_len/2; 0; 0];
